8769e38f211e65f1e480c5b6ae38184a4a5c57a7
Robert Resendez <grafx2g@gmail.com>
2024-01-05 09:39:18 -0800

    bitwiseOperator

--- BitwiseOperator.js
+++ BitwiseOperator.js
@@ -1,6 +1,6 @@
 const myString = "123";
 
-/* ~~ double tilda Bitwise (~) not operator converts to negative number, double negation makes it positive number */
+/* ~~ double tilda Bitwise (~) not operator converts to negative number, double negation (~~) makes it positive number */
 const myInt = ~~myString;
 
 console.log(myInt);
