0d2cfb459b28759e054518286c266e9de7bc7751
Robert Resendez <grafx2g@gmail.com>
2024-01-04 21:04:08 -0800

    working on arrays

--- arrays.js
+++ arrays.js
@@ -1,10 +1,10 @@
-// empty array
+// Creating an Empty Array
 const myArr = [];
 
-// indexing starts at 0
+// indexing starts at 0, commas to separate them
 const myArr1 = [1, 2, 3, 4, 5]
 
-// pulling out data from the array
+// pulling out data from the array using [] brackets
 const thirdElement = myArr1[2]; // returns 3
 
 console.log(thirdElement);
@@ -18,23 +18,25 @@ console.log(myArr2[0]);
 console.log(myArr2[1]);
 console.log(myArr2[2]);
 
-// Length Property
+console.log("================ Length =====================");
+
+// Length Property represents the number of elements in an Array
 const x = myArr1.length;
 const y = myArr2.length;
 
-console.log(x);
-console.log(y);
+console.log("x:", x);
+console.log("y:", y);
 
 // Array Methods
 
 let myArr3 = [1, 2, 3, 4, 5, 6]
 // retrieving data using square brackets [] is called indexing
-console.log(myArr3[3]) 
+console.log("returns:", myArr3[3]) 
 
 
 /* 
 
-Adding Elements 
+Adding Elements - using push() method
 
 */
 
@@ -53,3 +55,85 @@ console.log(myArr4);
 myArr4.unshift("beginning of Array");
 
 console.log(myArr4);
+
+
+console.log("=====================================");
+
+/*
+
+You can append a new value by Indexing:
+
+ */
+
+let myArr5 = [1, 2];
+myArr5[2] = 3;
+myArr5[3] = 4;
+
+console.log(myArr5);
+
+/* 
+
+indexing allows you to assign a value to a specified index, 
+reassign a previous value, and more:
+
+*/
+
+
+myArr5[0] = 4;
+
+console.log(myArr5);
+
+console.log("=====================================");
+
+let myArr6 = [1, 2];
+
+myArr6[myArr6.length] = "indexing 1";
+console.log(myArr6);
+
+myArr6.push("pushing 1");
+console.log(myArr6);
+
+myArr6[myArr6.length] = "indexing 2";
+console.log(myArr6);
+
+myArr6.push("pushing 2");
+console.log(myArr6);
+
+console.log("=====================================");
+
+/*
+
+Deleting Elements - using pop()
+
+*/
+
+let myArr7 = [11, 22, 33, 44];
+console.log("Array:", myArr7);
+
+let myNewArray = myArr7.pop();
+console.log(myNewArray);
+console.log("Array after pop:", myArr7);
+
+console.log("=====================================");
+
+const animals = ["Dogs", "Cats"];
+console.log(animals);
+const count = animals.pop();
+console.log(count);
+console.log(animals);
+
+console.log("=====================================");
+
+/*
+Little Test Array 
+Output: Bobby, 27, Bob, 21
+*/
+
+let myArr8 = ["Bobby", 25, "Bob", 16];
+
+myArr8.pop();
+console.log(myArr8);
+myArr8.push(21);
+myArr8[1] = 27;
+
+console.log(myArr8);
