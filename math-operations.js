efb23128c067e00e6fbbd05b707f79a40ab38a23
Robert R <grafx2g@gmail.com>
2024-01-09 08:07:49 -0800

    math operations

---
+++ math-operations.js
@@ -0,0 +1,40 @@
+ /*
+ 
+(Priority of Execution of Operations)
+Exponent ( ** )
+Reminder, or Modulo ( % )
+Division ( / )
+Multiplication ( * )
+Subtraction ( - )
+Addition ( + )
+ 
+ */
+
+// Addition and Subtraction
+
+console.log(25 + 13); // output: 38
+console.log(37 - 2); // output: 35
+
+let a = 25, b = 23;
+
+console.log(a + b); // output: 48
+console.log(a - b); // output: 2
+
+
+// Multiplication and Division
+
+console.log(12 * 3); // output: 36
+console.log(12 / 3); // output: 4
+console.log(273 / 23); // output: 11.869565217391305
+
+let c = 77, d = 11;
+console.log(c * d);
+console.log(c / d);
+
+// Remainder (Modulo)
+
+console.log(77 % 10); // output: 7
+console.log(25 % 11); // output: 3
+
+let e = 27, f = 21;
+console.log(e % f); // output: 6
