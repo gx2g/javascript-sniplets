04bdfab28fe81c8466aa6fb592176537c6f70f14
Robert Resendez <grafx2g@gmail.com>
2024-01-14 17:51:51 -0800

    comparison operations js

---
+++ comparison-operations.js
@@ -0,0 +1,41 @@
+/*
+
+Comparisons are used to define 
+conditions, perform checks, 
+and make decisions in JavaScript. 
+These operations return a boolean 
+value (true or false), which other 
+operators use to determine certain actions.
+
+
+Equal to (==);
+Strict Equal To (===);
+
+Not Equal To (!=);
+Strict Not Equal To (!==);
+
+Greater Than (>);
+Greater Than or Equal To (>=);
+
+Less Than (<);
+Less Than or Equal To (<=);
+
+
+Equal and Not Equal
+The Equal To operation (==) returns true 
+if the first value equals the second value 
+and false otherwise.
+
+Conversely, the Not Equal To operation (!=) 
+returns true if the values are unequal.
+
+*/
+
+console.log("5 == 5 is", 5 == 5);
+console.log("5 == 4 is", 5 == 4);
+console.log("5 == 4.99 is", 5 == 4.99);
+
+console.log("Not equal to:");
+console.log("5 != 5 is", 5 != 5);
+console.log("5 != 4 is", 5 != 4);
+console.log("5 != 4.99 is", 5 != 4.99);
