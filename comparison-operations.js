9ccdcf1878dec82ce17756f4710394f048f9c582
Robert Resendez <grafx2g@gmail.com>
2024-01-14 20:09:58 -0800

    comparison operations js

--- comparison-operations.js
+++ comparison-operations.js
@@ -29,6 +29,14 @@ and false otherwise.
 Conversely, the Not Equal To operation (!=) 
 returns true if the values are unequal.
 
+Equal and Not Equal
+
+The Equal To operation (==) returns true if the 
+first value equals the second value and false otherwise.
+
+Conversely, the Not Equal To operation (!=) returns true 
+if the values are unequal.
+
 */
 
 console.log("5 == 5 is", 5 == 5);
@@ -39,3 +47,66 @@ console.log("Not equal to:");
 console.log("5 != 5 is", 5 != 5);
 console.log("5 != 4 is", 5 != 4);
 console.log("5 != 4.99 is", 5 != 4.99);
+
+
+/* (NOTE)
+The = operator is for assignment, while == is for comparison. 
+Be careful to distinguish them.
+*/
+
+/* 
+
+Strict comparison
+
+ Strict comparison checks the types of values and values. 
+ The Strict Equal To (===) operator returns true - if both the 
+ values and their types are the same, while !== returns true 
+ if they are different.
+
+ */
+
+console.log("5 === 5 is", 5 === 5);
+console.log("5 === '5' is", 5 === '5');
+console.log("5 == '5' is", 5 == '5');
+
+console.log("5 !== 5 is", 5 !== 5);
+console.log("5 !== '5' is", 5 !== '5');
+console.log("5 != '5' is", 5 != '5');
+
+console.log("Type of 5 is", typeof(5));
+console.log("Type of '5' is", typeof('5'));
+
+/*
+
+Greater and Less
+
+The Greater Than (>) operator returns true if the first value 
+exceeds the second. Conversely, the Less Than (<) operator returns 
+true if the first value is less than the second.
+
+*/
+
+console.log("5 > 5 is", 5 > 5);
+console.log("5 > 4.99 is", 5 > 4.99);
+console.log("5 > 5.0000001 is", 5 > 5.0000001);
+
+console.log("5 < 5 is", 5 < 5);
+console.log("5 < 4.99 is", 5 < 4.99);
+console.log("5 < 5.0000001 is", 5 < 5.0000001);
+
+/*
+
+Greater/Less Than or Equal to
+The Greater Than or Equal To (>=) and Less Than or Equal To (<=) operators 
+combine the corresponding comparison operators with Equal To (==). 
+They return true if either of the conditions is satisfied.
+
+*/
+
+console.log("5 >= 5 is", 5 >= 5);
+console.log("5 >= 4.99 is", 5 >= 4.99);
+console.log("5 >= 5.0000001 is", 5 >= 5.0000001);
+
+console.log("5 <= 5 is", 5 <= 5);
+console.log("5 <= 4.99 is", 5 <= 4.99);
+console.log("5 <= 5.0000001 is", 5 <= 5.0000001);
