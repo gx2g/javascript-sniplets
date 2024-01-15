d02baac7423167940ca0d8c76f0f8ef0a52db69d
Robert Resendez <grafx2g@gmail.com>
2024-01-14 17:27:47 -0800

    assignment operations and increment decrement js

---
+++ assignment-operations.js
@@ -0,0 +1,31 @@
+/*
+Operations with Assignment
+
+Addition Assignment (+=);
+Substraction Assignment (-=);
+Multiplication Assignment (*=);
+Division Assignment (/=);
+Remainder (Modulo) Assignment (%=);
+Exponentiation Assignment (**=);
+
+*/
+
+let a = 17;
+
+a += 5; // same as a = a + 5;
+console.log(a); // answer: 22
+
+a -= 6; // a = a - 6
+console.log(a); // answer: 16
+
+a *= 6; // a = a * 6
+console.log(a); // answer: 96
+
+a /= 6; // a = a / 6
+console.log(a); // answer: 16
+
+a %= 6; // a = a % 6
+console.log(a); // answer: 4
+
+a **= 6; // a = a ** 6
+console.log(a); // answer: 4096
---
+++ increment-decrement.js
@@ -0,0 +1,46 @@
+/*
+Increment and Decrement are operations 
+used to increase or decrease a 
+variable's value by 1.
+
+
+*/
+
+let a = 0;
+
+a++;
+
+console.log(a); // answer: 1
+
+a++;
+
+console.log(a); // answer: 2
+
+a++; 
+
+console.log(a); // answer: 3
+
+/* 
+
+The expression a++ is
+equivalent to a = a + 1 or a += 1.
+
+*/
+
+let a = 5;
+
+a--;
+console.log(a); // answer: 4
+
+a--;
+console.log(a); // answer: 3
+
+a--;
+console.log(a); // answer: 2
+
+/*
+
+The expression a-- is 
+equivalent to a = a - 1 or a -= 1.
+
+*/
