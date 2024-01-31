d8c4b86f2084e058dd7f060fd2a736a50d0fa542
Robert Resendez <grafx2g@gmail.com>
2024-01-31 07:17:19 -0800

    string concatenation

---
+++ string-concatenation.js
@@ -0,0 +1,25 @@
+/*
+
+String Concatenation
+
+combine two strings with the + operator.
+
+
+*/
+
+let wordOne = "Hello";
+let wordTwo = "World";
+
+console.log(wordOne + wordTwo);
+
+/*
+You can see that concatenation creates a new string 
+by combining two words.
+
+(Note): String concatenation does not automatically add spaces. 
+You need to manually include spaces like string1 + " " + string2.
+
+*/ 
+
+console.log(wordOne + " " + wordTwo);
+console.log(wordOne + ", " + wordTwo + "!");
