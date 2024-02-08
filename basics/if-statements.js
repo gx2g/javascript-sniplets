ffae39d8a81b1bcf1919d74bb84cb862bea80f8f
gx2g <grafx2g@gmail.com>
2024-02-07 22:03:08 -0800

    if statements

--- basics/if-statements.js
+++ basics/if-statements.js
@@ -15,21 +15,21 @@ if(false){
 
 // expression as well as value can be considered. 
 
-let a = 935;
+let userLikes = 935;
 
-if (a > 17) {
+if (userLikes > 17) {
   console.log("The variable is greater than 17");
 }
 
-if (a > 235124) {
+if (userLikes > 235124) {
   console.log("The variable is greater than 235124");
 }
 
-if (a > 0) {
+if (userLikes > 0) {
   console.log("The variable is greater than 0");
 }
 
-if (a < 0) {
+if (userLikes < 0) {
   console.log("The variable is less than 0");
 }
 
@@ -38,7 +38,7 @@ if (a < 0) {
 
 let oranges = 5;
 let apples = 3;
-let fruit;
+let fruit; // variable no assignment
 
 if (oranges > 0 && apples > 0) {
   fruit = oranges - apples;
@@ -53,14 +53,44 @@ if (oranges > 2 && apples > 2) {
 console.log(oranges, apples, fruit);
 
 
-let gameScore = 199;
 
-if(gameScore >= 100){
+// Multiple Conditions
+
+let cardPlayer1 = 11;
+
+if (cardPlayer1 > 15) {
+  console.log('greater than 15');
+}
+
+if (cardPlayer1 > 10) {
+  console.log('greater than 10');
+}
+
+if (cardPlayer1 > 5) {
+  console.log('greater than 5');
+}
+
+if (cardPlayer1 > 0) {
+  console.log('greater than 0');
+}
+
+/* 
+in this senerio we have multiple true statements, 
+may not be the output we want. maybe we want to run only one statement
+*/
+
+
+// else if, breaks out if first true or last else if is false 
+let gameScore = 801;
+
+if(gameScore >= 1000){
     console.log("greater then = 100")
-} else if (gameScore >= 101) {
+} else if (gameScore <= 88) {
     console.log("first else if statement")
 } else if (gameScore >= 800) {
     console.log("last else if statement")
 } else {
     console.log("root else statement")
 }
+
+// play around with the numbers to get different condition statements
