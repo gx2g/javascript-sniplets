e50495d6809d91fa66dfa0be1ee4edd59d683285
Robert R <grafx2g@gmail.com>
2024-02-09 11:27:58 -0800

    destructuring json objects with variable assignment and default variable assignment when null

--- destructuring-json.js
+++ destructuring-json.js
@@ -1,8 +1,7 @@
 
-
-
-
 // 1.) Extract fields from JSON Object and store them in a variable
+
+
 const person1 = {
     userName: "Code Bobby",
     skill: "Advanced Javascript"
@@ -13,39 +12,44 @@ const {userName, skill} = person1
 console.log(userName);
 console.log(skill);
 
+console.log("===================================================");
+
 // 2. Extract fields from JSON Object and store them in a new variable)
 const person2 = {
-    userName: "Code BOB",
-    skill: "watching TV"
+    normalUser: "Code BOB",
+    normalskill: "watching TV"
 }
 
-const {userName: loserName, skill: loserSkill} = person2
+const {normalUser: loserName, normalskill: loserSkill} = person2
 
 console.log(loserName);
 console.log(loserSkill);
 
+console.log("===================================================");
+
 // 3.) Extract fields from JSON Object and store them in variable as well as create default value if field null
 
-const person3 = {
-    playerName: "Code BOB"
+const player = {
+    playerName: "Roberto Codes"
 }
 
 const {
-    playerName = "Roberto Codes",
-    playerSkill = "passing default Value Here"
-} = person3
+    playerName: playerID,
+    playerSkill: playerSkill = "passing default Value Here"
+} = player
 
-console.log(userName);
+console.log(playerID);
 console.log(playerSkill);
 
+console.log("===================================================");
 // 4.) Extract fields from JSON Object and store them to new variable as well as create default value if field null
 
-const workerName = { 
+let workerName = { 
     employeeName: "Robi Rob" 
 }
 
-const {
-    workerName: workerID = "Bob Fuegos", // new variable assignment workerID
+let {
+    employeeName: workerID = "Bob Fuegos", // new variable assignment workerID
     workerSkill: workerTitle = "fire starter" // default new variable assignment workerTitle
 } = workerName
 
