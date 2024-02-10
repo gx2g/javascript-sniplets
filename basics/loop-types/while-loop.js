e19d3c98790e6ccd74f5442801eaa17a9fa79f6d
gx2g <grafx2g@gmail.com>
2024-02-09 22:43:24 -0800

    loop types, while loop

---
+++ basics/loop-types/while-loop.js
@@ -0,0 +1,13 @@
+/*
+
+The while loop checks the condition before executing the code block. 
+If the condition is initially false, the code block will not be executed.
+
+*/
+
+let a = 0; 
+
+while(a <= 10) {
+    console.log("a = ", a);
+    a++;
+}
