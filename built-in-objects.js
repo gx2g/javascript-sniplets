423c979239fc260c24476951185783d5760cd0d3
Robert Resendez <grafx2g@gmail.com>
2024-02-11 13:45:59 -0800

    built in objects

--- built-in-objects.js
+++ built-in-objects.js
@@ -11,6 +11,9 @@ console.log(Math.random());
 // create a random number between 0 and 50
 console.log(Math.random() * 50);
 
+// create a random number between 1 and 50
+console.log((Math.random() * 50) +1);
+
 // Math.floor() makes whole number, notice how to wrap objects with method calls
 console.log(Math.floor(Math.random() * 50));
 
@@ -28,3 +31,18 @@ console.log(Math.abs("Hello String"));
 
 // The Math.cbrt() method returns the cubic root of a number.
 console.log(Math.cbrt(125));
+
+// The Math.floor() method rounds a number DOWN to the nearest integer.
+console.log(Math.floor(1.7));
+
+// The Math.max() method returns the number with the highest value.
+console.log(Math.max(0, 150, 30, 20, 38));
+
+// The Math.min() method returns the number with the lowest value.
+console.log(Math.min(0, 150, 30, 20, 38));
+
+// Math.PI returns PI (the ratio of a circle's area to the square of its radius, approximately 3.14)
+console.log(Math.PI);
+
+// The Math.pow() method returns the value of x to the power of y (xy).
+console.log(Math.pow(2, 2));
