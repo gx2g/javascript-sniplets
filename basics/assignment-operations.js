b55fc35694d474c7e8135558094624f618b4ffa4
Robert R <grafx2g@gmail.com>
2024-02-12 08:43:50 -0800

    variables and assignment operation

--- basics/assignment-operations.js
+++ basics/assignment-operations.js
@@ -29,3 +29,21 @@ console.log(a); // answer: 4
 
 a **= 6; // a = a ** 6
 console.log(a); // answer: 4096
+
+
+
+// Mathematical Assignment Operators practice
+
+let levelUp = 10;
+// levelUp += 5;
+let powerLevel = 9001;
+// powerLevel -= 100;
+let multiplyMe = 32;
+// multiplyMe  *= 11;
+let quarterMe = 1152;
+// quarterMe /= 4;
+
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+// Let variables can have reassignment
+
+let meal = 'Enchiladas';
+console.log(mail); // output: Enchiladas
+mail = 'Burrito';
+console.log(mail); // output: Burrito
+
+
+// (Let or Var) declare a variable without assigning the variable a value
+let price;
+console.log(price); // output: undefined
+price = 350;
+console.log(price); // output: 350
+
+// const was introduced in ES6
+const myName = 'Gilberto';
+console.log(myName);
