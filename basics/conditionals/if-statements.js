a8877f987a730378f3bb8f52f627b904fe02cc27
Robert Resendez <grafx2g@gmail.com>
2024-02-18 21:56:47 -0800

    conditionals

--- basics/conditionals/if-statements.js
+++ basics/conditionals/if-statements.js
@@ -360,3 +360,25 @@ switch (groceryItem1) {
 }
 
 // Prints 'Papayas are $1.29'
+
+
+/*
+
+*/
+
+let athleteFinalPosition = 'first place';
+
+switch(athleteFinalPosition){
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
+}
