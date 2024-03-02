d98b8fec6757159fab9466876e373ffd07a7f196
gx2g <grafx2g@gmail.com>
2024-03-02 00:09:37 -0800

    singleton pattern

--- design-patterns/singleton-pattern.js
+++ design-patterns/singleton-pattern.js
@@ -6,7 +6,8 @@ comes in handy when you want to Limit the number of instances of an object to at
 Write a program that manages processes. 
 
 two main components, process and process manager.
-You can have one ore more processes but you want to limit the instance of process manager to 1. 
+You can have one ore more processes but you want to limit the instance of 
+process manager to 1. 
 
 */
 
@@ -16,42 +17,55 @@ function Process(state) {
 }
 
 // set to invoke function expression, create process manager
-const Singleton = (function(){
+const processManagerSingleton = (function(){
 
-	// inside we put any object constructor for any of the objects we want to limit.
-	// in this case we are going to limit the number of instances of the process manager. 
-	function processManager() 
-	{
+	/* 
+	inside we put any object constructor for any of the objects we want to limit.
+	in this case we are going to limit the number of instances of the process manager. 
+	*/
+	 function processManager() {
 		this.numProcess = 0;
 	}
 
 	// store a reference of the only instance we are going to be allowed to use.
-	let pManager // only instance allowed to use
+	let pManager // only instance allowed to use, not defined
+	
+	console.log(pManager) // test to see if pManager is declared
 
-	// not exposed to the outside, local to singleton
-	// create process manager, inside going to assign pManager = to process manager and return pManager
-
-	function createProcessManager()
-	{
+	/* 
+	not exposed to the outside, local to singleton create process manager, inside 
+	going to assign pManager = to process manager and return pManager
+	*/
+	function createProcessManager(){
+		// create a new process manager instnace and return it as pManager
 		pManager = new processManager()
 		return pManager
 	}
-
+	// we are exposing an object that has a method called getProcessManager
 	return {
+		// call method
 		getProcessManager:()=>{
+			// checking if instance pManager is set to null
 			if(!pManager)
-			
+				// create a new instance of our pManager, set pManager
 				pManager = createProcessManager()
+				// if it's already set just return our instance of pManager
 				return pManager
 		}
 	}
 }
-)()
+)();
 
-const processManager = Singleton.getProcessManager()
-const processManager2 = Singleton.getProcessManager()
+const processManager = processManagerSingleton.getProcessManager();
+const processManager2 = processManagerSingleton.getProcessManager();
+const processManager3 = processManagerSingleton.getProcessManager();
 
 console.log(processManager === processManager2)
+console.log(processManager2 === processManager3)
+
 console.log(processManager)
 console.log(processManager2)
+
+let numProcess = 5
+console.log(numProcess)
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
