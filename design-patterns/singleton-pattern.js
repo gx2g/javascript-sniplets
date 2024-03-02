93897329f56c005465bd8c138edc5f6b3f42fce0
gx2g <grafx2g@gmail.com>
2024-03-01 20:53:58 -0800

    singleton pattern

--- design-patterns/singleton-pattern.js
+++ design-patterns/singleton-pattern.js
@@ -10,30 +10,48 @@ You can have one ore more processes but you want to limit the instance of proces
 
 */
 
+// create a process
 function Process(state) {
 	this.state = state
 }
 
-// set to invoke function expression
-const ProcessManagerSingleton = (function(){
+// set to invoke function expression, create process manager
+const Singleton = (function(){
 
 	// inside we put any object constructor for any of the objects we want to limit.
 	// in this case we are going to limit the number of instances of the process manager. 
-	function ProcessManager() {
+	function processManager() 
+	{
 		this.numProcess = 0;
 	}
 
 	// store a reference of the only instance we are going to be allowed to use.
-	let pManager
+	let pManager // only instance allowed to use
+
 	// not exposed to the outside, local to singleton
-	function createProcessManager() {
-		pManager = new ProcessManager ()
+	// create process manager, inside going to assign pManager = to process manager and return pManager
+
+	function createProcessManager()
+	{
+		pManager = new processManager()
 		return pManager
 	}
 
+	return {
+		getProcessManager:()=>{
+			if(!pManager)
+			
+				pManager = createProcessManager()
+				return pManager
+		}
+	}
+}
+)()
 
+const processManager = Singleton.getProcessManager()
+const processManager2 = Singleton.getProcessManager()
 
-})();
-
-
+console.log(processManager === processManager2)
+console.log(processManager)
+console.log(processManager2)
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
