eee0ecf38ce80ef0daa696314718a7a3bd0f6dc4
gx2g <grafx2g@gmail.com>
2024-03-02 10:16:13 -0800

    strategy pattern

---
+++ design-patterns/strategy-pattern.js
@@ -0,0 +1,47 @@
+/*
+
+Strategy Pattern 
+
+referrs to encapsulating a group of data or functions
+
+what is Encapsulation in OOP?
+
+Encapsulation is a way to restrict the direct access to some 
+components of an object, so users cannot access state values for 
+all of the variables of a particular object. Encapsulation can be
+used to hide both data members and data functions or methods associated
+with an instantiated class or object.
+
+
+- In object-oriented computer programming (OOP) languages, the notion of encapsulation 
+  (or OOP Encapsulation) refers to the bundling of data, along with the methods 
+  that operate on that data, into a single unit.
+
+- Containers are just one example of encapsulation in coding where data and methods 
+  are bundled together into a single package.
+
+- A key benefits to hiding information about attributes and methods using encapsulation 
+  in programming is that it prevents other developers from writing scripts or APIs that 
+  use your code.
+
+- Sumo Logic complements your existing cyber security measure with cutting-edge 
+  threat detection and security analytics powered by artificial intelligence.
+
+
+Benefits of encapsulation programming
+
+Encapsulation in programming has a few key benefits. These include:
+
+Hiding data: 
+Users will have no idea how classes are being implemented or stored. 
+All that users will know is that values are being passed and initialized.
+
+More flexibility: 
+Enables you to set variables as read or write-only. 
+Examples include: setName(), setAge() or to set variables as write-only 
+then you only need to omit the get methods like getName(), getAge() etc.
+
+Easy to reuse: 
+With encapsulation, it's easy to change and adapt to new requirements
+
+*/
