2f75dd55ec50b5391044eafa09a26ae5bae86089
gx2g <grafx2g@gmail.com>
2024-03-19 14:44:57 -0700

    object, dot notation, square notation in nested data

--- data-structures/objects.js
+++ data-structures/objects.js
@@ -57,13 +57,30 @@ const classicCar = {
 
 };
 
+
+/*
+. notation is most common way to access
+[] square brackets is another way, his syntax involves 
+specifying the object's name, followed by square brackets 
+with the property name as a string inside them.
+
+Essential in cases where the property name is not known in 
+advance or is stored in a variable, such as a function parameter
+
+This method provides the property name as a string within square 
+brackets. It allows for dynamic property access, which can be 
+helpful when dealing with more complex data.
+
+*/
+
 console.log(classicCar.make);
 console.log(classicCar["type@"]);
 console.log(classicCar._tires);
-
+console.log(classicCar["miles"]);
 /*
 
 Nested Properties
+specifications and owner properties are objects themselves. 
 
 */
 
@@ -83,4 +100,58 @@ const computer = {
 };
 
 console.log(computer.brand);
-console.log(computer.specifications.processor)
+console.log(computer.specifications.processor);
+
+
+/*
+
+Create a JavaScript object called informationCard representing basic personal information. Inside this object, include a nested object with contact details, such as a home address and phone number. The object should have the following structure:
+
+Information card (outer object):
+
+First Name (e.g., "Alice");
+Last Name (e.g., "Smith");
+Age (e.g., 25);
+Contact Details (nested object)
+Contact Details (nested object):
+
+Home Address (e.g., "123 Main St, Apt 4B");
+Phone Number (e.g., "555-123-4567");
+
+*/
+
+const informationCard = {
+    firstName: "Alice",
+    lastName: "Smith",
+    age: 25,
+    contactDetails: {
+      homeAddress: "123 Main St, Apt 48",
+      phoneNumber: "555-123-4567",
+    },
+  };
+
+
+  /*
+  
+  access using . dot noation as well as [] square bracket notation 
+  to display nested data.
+  
+  */
+
+
+  const userShippingInfo = {
+    name: "Alice",
+    address: {
+      street: "123 Main Street",
+      city: "Wonderland",
+      postalCode: "12345",
+    },
+    phoneNumbers: {
+      home: "555-123-4567",
+      work: "555-987-6543",
+    },
+  };
+
+  console.log(userShippingInfo.address.postalCode); // not notation
+  console.log(userShippingInfo["address"]["postalCode"]); // square notation
+  
