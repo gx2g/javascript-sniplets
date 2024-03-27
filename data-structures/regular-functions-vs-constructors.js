d817e35efbdbfbbd0d86fed540fb6c66f22dee69
Robert Resendez <grafx2g@gmail.com>
2024-03-26 21:12:21 -0700

    regular functions vs constructors

---
+++ data-structures/regular-functions-vs-constructors.js
@@ -0,0 +1,39 @@
+/*
+
+Regular Functions vs Constructors
+https://youtu.be/fBpPfPjxOhc?si=7PrJJkBCVbU_Yn5b
+
+*/
+
+let motorBike = {
+    type: "Street Bike",
+    color: "Black",
+    model: 500,
+    make: "Yamaha"
+}
+
+motorBike.type = "Skooter";
+motorBike.color = "white";
+motorBike.model = 200;
+motorBike.make = "viper";
+
+console.log(motorBike);
+
+
+function createMoterBike(type, color, model, make){
+    
+    let newMotorBike = {};
+    
+    newMotorBike.type = type;
+    newMotorBike.color = color;
+    newMotorBike.model = model;
+    newMotorBike.make = make;
+
+    return newMotorBike;
+
+}
+
+let motorBike1 = createMoterBike("custom", "navy blue", 547, "harley");
+console.log(motorBike1);
+
+console.log(createMoterBike("sports bike", "green", 2200, "honda"));
