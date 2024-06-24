601c9e62740defc373432abf39e3ecfd3a596c3c
Robert R <grafx2g@gmail.com>
2024-06-24 08:50:48 -0700

    shift operator example

---
+++ basics/shift-operator.js
@@ -0,0 +1,22 @@
+/*
+
+Shift Operator shifts the bits of a number to the right
+
+*/ 
+
+let num1 = 10;
+let num2 = num1 >> 1;
+console.log(num2);
+
+/* 
+
+Bit Representation of 10 
+
+1010 - remove right 0 bit
+0101 - add bit in front 
+
+0101 = 5
+
+*/
+
+
